2ace7490afd974c0d3247655361dad50c052cd23
Yu Feng <mbiiweb@mbii.localdomain>
2014-02-03 23:49:03 +0000

    preparing for public data release.

--- python/makedata.py
+++ python/makedata.py
@@ -12,7 +12,7 @@ cub = Cubenoid(M, [0, 0, 0], [boxsize, boxsize, boxsize],
         center=0, neworigin=0)
 width = cub.newboxsize[1]
 height = cub.newboxsize[0]
-print cub.edges
+print cub.newboxsize
 from sys import argv
 
 snapid = int(argv[1])
@@ -34,71 +34,91 @@ def main():
     metadata['redshift'] = snap.redshift
     numpy.savez(os.path.join(output, 'metadata.npz'), metadata=metadata)
 
-#    makegroup(snap)
-#    makesubhalo(snap)
-    updatesubhalo(snap)
+    makegroup(snap, update=False)
+    makesubhalo(snap, update=False)
 
-def makegroup(snap):
+def makegroup(snap, update=False):
     g = snap.readgroup()
     outarray = dirarray(os.path.join(output, 'group'), 
         dtype=[('x', 'f4'), 
             ('y', 'f4'), 
             ('mass', 'f4'), 
+            ('pos', ('f4', 3)), 
+            ('massbytype', ('f4', 6)), 
+            ('lenbytype', ('u4',6)), 
             ('Nsubhalo', 'u4'),
             ('firstsubhalo', 'u4'),
             ], shape=len(g), mode='w+')
 
-    x, y, z = g['pos'].T.copy()
-    cub.apply(x, y, z)
-    outarray['y'] = x / width
-    outarray['x'] = y / width
+    if not update:
+        x, y, z = g['pos'].T.copy()
+        cub.apply(x, y, z)
+        outarray['y'] = x / width
+        outarray['x'] = y / width
+    
+    outarray['pos'] = g['pos']
     outarray['mass'] = g['mass']
+    outarray['massbytype'] = g['massbytype']
+    outarray['lenbytype'] = g['lenbytype']
     outarray['Nsubhalo'] = g['nhalo']
     outarray['firstsubhalo'] = numpy.concatenate([[0], g['nhalo'].cumsum()])[:-1]
     print outarray[0]
 
-def makesubhalo(snap):
+def makesubhalo(snap, update=False):
     sub = snap.load('subhalo', 'tab')
     g = snap.readgroup()
     type = snap.load('subhalo', 'type')
     mask = ~numpy.isnan(sub['mass'])
     outarray = dirarray(os.path.join(output, 'subhalo'), 
-        dtype=[('x', 'f4'), 
+        dtype=[
+            ('x', 'f4'), 
             ('y', 'f4'), 
+            ('pos', ('f4', 3)), 
             ('size', 'f4'),
             ('groupid', 'u4'),
             ('iscentral', '?'),
             ('mass', 'f4'),
-            ('gasmass', 'f4'),
-            ('darkmass', 'f4'),
-            ('starmass', 'f4'),
-            ('bhmass', 'f4'),
+            ('massbytype', ('f4', 6)), 
+            ('lenbytype', ('u4', 6)), 
+            ('vdisp', 'f4'),  
+            ('vcirc', 'f4'),  
             ('rcirc', 'f4'),
-            ('SDSS.i', 'f4'),
-            ('SDSS.r', 'f4'),
-            ('SDSS.g', 'f4'),
-            ('SDSS.u', 'f4'),
-            ('SDSS.z', 'f4'),
+            ('SDSS.i', 'f4'), ('SDSS.r', 'f4'), ('SDSS.g', 'f4'), ('SDSS.u', 'f4'), ('SDSS.z', 'f4'),
+            ('FAKE.1500', 'f4'), ('FAKE.2000', 'f4'), ('FAKE.2500', 'f4'), 
+            ('FAKE.Un', 'f4'), ('FAKE.V', 'f4'), ('FAKE.Vn', 'f4'), ('FAKE.Vw', 'f4'),
+            ('GALEX.FUV', 'f4'), ('GALEX.NUV', 'f4'),
+            ('UKIRT.WFCAM.H', 'f4'), ('UKIRT.WFCAM.J', 'f4'), ('UKIRT.WFCAM.K', 'f4'), ('UKIRT.WFCAM.Y', 'f4'),
             ], shape=mask.sum(), mode='w+')
 
-    massbytype = sub['massbytype'][mask]
 
-    x, y, z = numpy.float32(sub['pos'].T)
-    x = x[mask]
-    y = y[mask]
-    z = z[mask]
-    cub.apply(x, y, z)
-    outarray['x'] = y / width
-    outarray['y'] = x / width
+    if not update:
+        x, y, z = numpy.float32(sub['pos'].T)
+        x = x[mask]
+        y = y[mask]
+        z = z[mask]
+        cub.apply(x, y, z)
+        outarray['x'] = y / width
+        outarray['y'] = x / width
+    
+    outarray['pos'] = sub['pos'][mask]
     outarray['mass'] = sub['mass'][mask]
     outarray['groupid'] = sub['groupid'][mask]
-    outarray['iscentral'] = outarray['mass'] > 0.1 * g['mass'][outarray['groupid']]
-    outarray['gasmass'] = massbytype[:, 0]
-    outarray['darkmass'] = massbytype[:, 1]
-    outarray['starmass'] = massbytype[:, 4]
-    outarray['bhmass'] = massbytype[:, 5]
+    massbytype = sub['massbytype'][mask]
+    outarray['iscentral'] = outarray['mass'] > 0.3 * g['mass'][outarray['groupid']]
+    outarray['massbytype'] = massbytype
     outarray['rcirc'] = sub['rcirc'][mask]
+    outarray['vcirc'] = sub['vcirc'][mask]
+    outarray['vdisp'] = sub['vdisp'][mask]
     outarray['size'] = outarray['rcirc'] / width * 10
+    for s in ['1500', '2000', '2500', 'Un', 'V', 'Vn', 'Vw']:
+        data = snap.load('subhalo', 'RfFilter/FAKE.FAKE/%s' %s)
+        outarray['FAKE.%s' %s] = data[mask]
+    for s in ['FUV', 'NUV']:
+        data = snap.load('subhalo', 'RfFilter/GALEX.GALEX/%s' %s)
+        outarray['GALEX.%s' %s] = data[mask]
+    for s in ['H', 'J', 'K', 'Y']:
+        data = snap.load('subhalo', 'RfFilter/UKIRT.WFCAM/%s' %s)
+        outarray['UKIRT.WFCAM.%s' %s] = data[mask]
     for s in 'irguz':
         data = snap.load('subhalo', 'RfFilter/SDSS.SDSS/%s' %s)
         outarray['SDSS.%s' %s] = data[mask]
@@ -106,30 +126,5 @@ def makesubhalo(snap):
     print outarray[0]
     print outarray.dtype
 
-def updatesubhalo(snap):
-    sub = snap.load('subhalo', 'tab')
-    type = snap.load('subhalo', 'type')
-    mask = type == 0
-    g = snap.readgroup()
-    outarray = dirarray(os.path.join(output, 'subhalo'), 
-        dtype=[('x', 'f4'), 
-            ('y', 'f4'), 
-            ('size', 'f4'),
-            ('groupid', 'u4'),
-            ('iscentral', '?'),
-            ('mass', 'f4'),
-            ('gasmass', 'f4'),
-            ('darkmass', 'f4'),
-            ('starmass', 'f4'),
-            ('bhmass', 'f4'),
-            ('rcirc', 'f4'),
-            ('SDSS.i', 'f4'),
-            ('SDSS.r', 'f4'),
-            ('SDSS.g', 'f4'),
-            ('SDSS.u', 'f4'),
-            ('SDSS.z', 'f4'),
-            ], shape=mask.sum(), mode='r+')
-
-    outarray['size'] = outarray['rcirc'] / width * 10
 
 main()
